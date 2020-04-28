ff28255d84f64b27714dfc5934d07ad287478acd
Kevin Zhao <kevinz6@illinois.edu>
2020-04-28 17:37:01 -0500

    kevin - few minor tweaks

--- app/src/main/java/com/example/dotandboxes/MenuActivity.java
+++ app/src/main/java/com/example/dotandboxes/MenuActivity.java
@@ -13,12 +13,15 @@ import android.widget.TextView;
 public class MenuActivity extends AppCompatActivity {
 
     //just a dummy variable to hold places for now
-    private boolean placeHolding = false;
+    //private boolean placeHolding = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
+        Intent i = getIntent();
+        TextView t = findViewById(R.id.nameDisplay);
+        Bundle b = i.getExtras();
+        t.setText(getResources().getString(R.string.display_user, (String) b.get("Username")));
         //this part will handle creating the new game
 
         findViewById(R.id.createGame).setOnClickListener(unused -> startActivity(
@@ -38,32 +41,32 @@ public class MenuActivity extends AppCompatActivity {
     private void connect() {
         findViewById(R.id.OpenLobbyGroup).setVisibility(View.GONE);
         findViewById(R.id.loadGroup).setVisibility(View.GONE);
-        //more implementation when we connect to firebase
-        if(!placeHolding) {
-            setUpUi();
-        }
-    }
-    private void setUpUi() {
-        //these are placeholder values that will be changed
-        String gameOwner = "placeHolder";
-        int[] boardSize = new int[] {2, 2};
 
+        //more implementation when we connect to firebase
+        String[] owners = new String[]{"placeHolder", "test", "subject 1"};
+        int[][] size = new int[][] {{2, 2}, {20, 20}, {100, 100}};
+        setUpUi(owners, size);
+    }
+    private void setUpUi(String[] gameOwner, int[][] boardSize) {
+        //these are placeholder values that will be changed
         findViewById(R.id.OpenLobbyGroup).setVisibility(View.VISIBLE);
 
         LinearLayout openLayout = findViewById(R.id.openGamesList);
+        openLayout.removeAllViews();
         openLayout.setVisibility(View.VISIBLE);
-        View chunk = getLayoutInflater().inflate(R.layout.chunk_open_lobby,
-                openLayout, false);
-        openLayout.addView(chunk);
-        Button join = chunk.findViewById(R.id.join_game);
-        join.setOnClickListener(v -> {
-            startActivity(new Intent(this, GameActivity.class));
-        });
-        TextView ownerLabel = chunk.findViewById(R.id.game_owner);
-        ownerLabel.setText(getResources().getString(R.string.game_owner, gameOwner));
-        TextView infoLabel = chunk.findViewById(R.id.game_size); // Shows both the user's role and the game mode
-        infoLabel.setText(getResources().getString(R.string.game_size, boardSize[0], boardSize[1]));
-        placeHolding = true;
+        for (int i = 0; i < gameOwner.length; i++) {
+            View chunk = getLayoutInflater().inflate(R.layout.chunk_open_lobby,
+                    openLayout, false);
+            openLayout.addView(chunk);
+            Button join = chunk.findViewById(R.id.join_game);
+            join.setOnClickListener(v -> {
+                startActivity(new Intent(this, GameActivity.class));
+            });
+            TextView ownerLabel = chunk.findViewById(R.id.game_owner);
+            ownerLabel.setText(getResources().getString(R.string.game_owner, gameOwner[i]));
+            TextView infoLabel = chunk.findViewById(R.id.game_size); // Shows both the user's role and the game mode
+            infoLabel.setText(getResources().getString(R.string.game_size, boardSize[i][0], boardSize[i][1]));
+        }
     }
 
 }
