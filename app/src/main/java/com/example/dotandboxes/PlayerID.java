4cdfea917a3f1c0d0da39e1b37981dec0488992b
davisp2 <davisp2@illinois.edu>
2020-05-02 13:34:55 -0500

    Davis - Created GameView, GameBoard, PlayerID. GameView uses canvas to draw board (I make some comment documentation soon). GameBoard has all the logic for the board state. PlayerID contains each player's color.

---
+++ app/src/main/java/com/example/dotandboxes/PlayerID.java
@@ -0,0 +1,9 @@
+package com.example.dotandboxes;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public final class PlayerID {
+    public static final int ONE_COLOR = Color.RED;
+    public static final int TWO_COLOR = Color.BLUE;
+}
