561ccb9c1171c9840a7c96c5085029b937646bbe
Kevin Zhao <kevinz6@illinois.edu>
2020-05-02 17:59:13 -0500

    kevin - firebase stuff not working rn but i am working on it

--- app/src/main/java/com/example/dotandboxes/MenuActivity.java
+++ app/src/main/java/com/example/dotandboxes/MenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.dotandboxes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,8 +12,14 @@ import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -58,16 +65,21 @@ public class MenuActivity extends AppCompatActivity {
         createLayout.addView(chunk);
         TextView ownerLabel =findViewById(R.id.create_game_owner);
         ownerLabel.setText(getResources().getString(R.string.game_owner, playerName));
+        newRoomRef = database.getReference("rooms/" + playerName);
+        newRoomRef.child("player1").setValue(playerName);
 
         SeekBar bar = findViewById(R.id.seekBar);
         TextView sizeDisplay = findViewById(R.id.sizeDisplay);
         sizeDisplay.setText(getResources().getString(R.string.game_size, 3, 3));
 
+        Intent intent = new Intent(getApplicationContext(), GameActivity.class);
         bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 int value = 3 + (progress);
                 sizeDisplay.setText(getResources().getString(R.string.game_size, value, value));
+                newRoomRef.child("size").setValue(value);
+                intent.putExtra("roomSize", value);
             }
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
@@ -77,9 +89,8 @@ public class MenuActivity extends AppCompatActivity {
             public void onStartTrackingTouch(SeekBar seekBar) {
             }
         });
-
-        newRoomRef = database.getReference("rooms/" + playerName + "/player1");
-        newRoomRef.setValue(playerName);
+        intent.putExtra("roomName", playerName);
+        startActivity(intent);
     }
 
 
@@ -88,19 +99,44 @@ public class MenuActivity extends AppCompatActivity {
         findViewById(R.id.loadGroup).setVisibility(View.GONE);
 
         //more implementation when we connect to firebase
-        String[] owners = new String[]{"placeHolder", "test", "subject 1"};
-        int[][] size = new int[][] {{2, 2}, {20, 20}, {100, 100}};
-        setUpUi(owners, size);
+        Map<String, Integer> openLobby = new HashMap<>();
+        openLobby.put("placeHolder", 3);
+        openLobby.put("test", 10);
+        openLobby.put("subject", 30);
+        setUpUi(openLobby);
     }
 
-    private void setUpUi(String[] gameOwner, int[][] boardSize) {
+    //not yet put to use 
+    private Map<String, Integer> getLobbies() {
+        Map<String, Integer> lobby = new HashMap<>();
+        lobbyRef = database.getReference("rooms");
+        lobbyRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> rooms = dataSnapshot.getChildren();
+                for(DataSnapshot snapshot: rooms) {
+                    lobby.put((String) snapshot.child("player1").getValue(),
+                            (Integer) snapshot.child("roomSize").getValue());
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //error
+            }
+
+        });
+        return lobby;
+    }
+
+
+    private void setUpUi(Map<String, Integer> lobby) {
         //these are placeholder values that will be changed
         findViewById(R.id.OpenLobbyGroup).setVisibility(View.VISIBLE);
 
         LinearLayout openLayout = findViewById(R.id.openGamesList);
         openLayout.removeAllViews();
         openLayout.setVisibility(View.VISIBLE);
-        for (int i = 0; i < gameOwner.length; i++) {
+        for (Map.Entry<String, Integer> room : lobby.entrySet()) {
             View chunk = getLayoutInflater().inflate(R.layout.chunk_open_lobby,
                     openLayout, false);
             openLayout.addView(chunk);
@@ -109,9 +145,9 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(new Intent(this, GameActivity.class));
             });
             TextView ownerLabel = chunk.findViewById(R.id.game_owner);
-            ownerLabel.setText(getResources().getString(R.string.game_owner, gameOwner[i]));
+            ownerLabel.setText(getResources().getString(R.string.game_owner, room.getKey()));
             TextView infoLabel = chunk.findViewById(R.id.game_size); // Shows both the user's role and the game mode
-            infoLabel.setText(getResources().getString(R.string.game_size, boardSize[i][0], boardSize[i][1]));
+            infoLabel.setText(getResources().getString(R.string.game_size, room.getValue(), room.getValue()));
         }
     }
 
